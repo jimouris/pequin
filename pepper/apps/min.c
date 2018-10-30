bfb896eb6147de960521ed490aefa1f8a249581b
Dimitris Mouris <jimouris@gmail.com>
2018-10-30 13:03:26 +0200

    Add simple min function to find minimum of list

---
+++ pepper/apps/min.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+
+#define SIZE 10
+
+struct In {
+    uint32_t array[SIZE];
+};
+
+struct Out {
+    uint32_t min;
+};
+
+/*
+  Search a sorted array and return the minimum value.
+*/
+void compute(struct In* input, struct Out* output){
+  uint32_t i, min;
+
+  // Search the array (linear time)
+  min = UINT32_MAX;
+  for(i = 0; i < SIZE; i++){
+    if (input->array[i] < min){
+      min = input->array[i];
+    }
+  }
+
+  output->min = min;
+}
